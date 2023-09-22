1a6a4464750a1f0b0572545e82981c7b6d91c3e7
Neal Meinke <40125744+nealm682@users.noreply.github.com>
2023-09-22 13:14:25 -0700

    Updated total score

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
